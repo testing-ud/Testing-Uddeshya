4bedf45dea554033a270d02a0e49855e02e438e0
Uddeshya Gupta <52238292+Ud101298@users.noreply.github.com>
2024-01-12 20:36:49 +0530

    Delete Indianops-main/sidebar directory

--- Indianops-main/sidebar/sidebar.js
+++
@@ -1,8 +0,0 @@
-// Responsive Sidenav
-const sideNav = document.getElementById("sidenav");
-const burgerMenu = document.getElementById("burgermenu");
-
-burgerMenu.addEventListener("click", function () {
-	sideNav.classList.toggle("active");
-	burgerMenu.classList.toggle("active");
-});
